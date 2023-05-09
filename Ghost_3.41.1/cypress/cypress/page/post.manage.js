3c6689b6c159fd2493773f9e7c8b08e42f206d83
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2023-05-08 22:52:24 -0500

    Se agrega eliminar post

--- Ghost_3.41.1/cypress/cypress/page/post.manage.js
+++ Ghost_3.41.1/cypress/cypress/page/post.manage.js
@@ -1,45 +1,62 @@
 class PostManage {
     
     createNewButton() {
-        return cy.get(".gh-canvas").contains("New post").click({ force: true });
+        return cy.get(".gh-canvas").contains("New post").click();
+    }
+
+    clickP() {
+        return cy.get(".gh-viewport  ").click();
+    }
+
+
+    clickPost() {
+        return cy.get(".f8>a").contains("Posts").click();
     }
 
     backPost() {
-        return cy.get(`a[href="#/posts/"]`).click({ force: true });
-    }
+        cy.go('back');
+    }    
     
     validateInList(value){
         return cy.get('h3').parent(cy.get(".content-list")).contains(value)
 
-        
-    }
-    fillTitle(value){
-        const field =  cy.get(`textarea[placeholder="Post title"]`);
-        field.type(value);    
-        return this;
     }
 
-    fillDescription(value){
-        const field =  cy.get(`div[data-placeholder="Begin writing your post..."]`);
-        field.clear();
-        field.type(value);
-    
+    fillTitle(value){
+        const field =  cy.get(`textarea[placeholder="Post Title"]`);
+        field.clear().type(value, { force: true });
         return this;
+    }
+    seeSettings(){
+        return cy.get(".post-settings").click({ force: true });
     }
 
     btnPublish(){
-        //return cy.get(`button[data-test-button="publish-flow"]`).click({ force: true });
+        return cy.get(".view-actions").contains("Publish").click({ force: true });
+    }
+
+    backDarfPost(){
+        return cy.get(".gh-nav-body").contains("Drafts").click();
     }
 
     btnConfirmPublish(){
-        //return cy.get(`button[data-test-button="confirm-publish"]`).click({ force: true });
+        return cy.get(".gh-publishmenu-footer").contains("Publish").click({ force: true });
     }
 
     editPost(value){
         return cy.get(".content-list").contains(value).click({ force: true });
     }
 
+    selectFirst(){        
+        return  cy.get('.posts-list>li>a').eq(1).click({ force: true })
+    }
 
+    deletePost(){        
+        return  cy.get('.settings-menu-container').contains("Delete").click({ force: true })
+    }
+    confirmDeletePost(){        
+        return  cy.get('.modal-footer').contains("Delete").click({ force: true })
+    }
   }
   
   module.exports = new PostManage();
