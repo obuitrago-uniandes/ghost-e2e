2fc45878b9919d4f8a5673542a118162af324e1e
Alejandra Uniandes <ma.ninog12@uniandes.edu.co>
2023-05-10 17:53:42 -0500

    Ajuste selectores de navegación

--- Ghost_3.41.1/cypress/cypress/e2e/post-section.cy.js
+++ Ghost_3.41.1/cypress/cypress/e2e/post-section.cy.js
@@ -1,14 +1,11 @@
 const loginPage = require("../page/login.page");
 const adminPage = require("../page/admin.page");
 const postManage = require("../page/post.manage");
-const screenShots = require("../page/screenshots")
 
 /// <reference types="cypress" />
 
 context("Navigation", () => {
   beforeEach(() => {
-    screenShots.incrementIndexScenario();
-    screenShots.resetIndexStep();
     cy.fixture("ghost.json").as("ghostData");
     cy.visit("/ghost/#/signin");
     cy.get("@ghostData").then((ghostData) => {
@@ -17,93 +14,85 @@ context("Navigation", () => {
         .fillPassword(ghostData.password)
         .submit();
     });
-    screenShots.screenShot();
     cy.wait(1000);
   });
+
+  
+  it('Scenario: No Permitir guardar borrador de post con titulo vacio', () => { 
+    cy.wait(1000);
+    //When I go section Post
+    adminPage.clickPostBtn()
+    cy.wait(1000);
+    postManage.createNewButton()
+    cy.wait(1000);
+    //And I back to list Post
+    postManage.backPost()
+    cy.go(-1)
+    cy.wait(1000);
+    //And I go to Draf
+    postManage.backDarfPost()
+    cy.wait(1000);
+  })
 
   it('Scenario: Crear post vacio', () => {
       cy.wait(1000);
       //When I go section Post
       adminPage.clickPostBtn()
-      screenShots.screenShot();
       cy.wait(1000);
       postManage.createNewButton()
-      screenShots.screenShot();
       cy.wait(1000);
       //And I enter title
-      postManage.fillTitle(' ');
-      screenShots.screenShot();
-      cy.wait(1000);
-      //And I back to list Post
-      postManage.clickP()
-      screenShots.screenShot();
-      
+      postManage.fillTitle('        ');
+      postManage.clickP();
   })
 
-//Se Crea
-  it('Scenario: Crear post', () => { 
+
+  it('Scenario: Crear post Borrador', () => {
     cy.wait(1000);
-    //When I go section pages
+    //When I go section Post
     adminPage.clickPostBtn()
-    screenShots.screenShot();
     cy.wait(1000);
     postManage.createNewButton()
-    screenShots.screenShot();
     cy.wait(1000);
+    //And I enter title
     postManage.fillTitle('Borrador');
-    screenShots.screenShot();
-    cy.go(-1)
-    cy.wait(1000);
-    //And I back to list Post
-    postManage.clickP()
-    screenShots.screenShot();
-    cy.wait(1000);
-
+    postManage.clickP();
   })
 
-  //Se publica
-  it('Scenario: Publicar publicar', () => { 
+
+
+  it('Scenario: Publicar Post', () => { 
     cy.wait(1000);
-    //When I go section pages
+      //When I go section Post
     adminPage.clickPostBtn()
-    screenShots.screenShot();
     cy.wait(1000);
     postManage.editPost('Borrador');
-    screenShots.screenShot();
-    cy.wait(1000);
     //And I click publish 
     postManage.btnPublish()
-    screenShots.screenShot();
     //And I click confirm publish 
     postManage.btnConfirmPublish()
-    screenShots.screenShot();
-    //And I back to list Post
-    postManage.clickP()
-    screenShots.screenShot();
-    postManage.backPost()
-    screenShots.screenShot();
     cy.wait(1000);
-
+    //Then I expect publish
+    postManage.backPost();
+    cy.go(-1)
+    cy.wait(1000);
   })
+
   //Se edita
   it('Scenario: Editar post', () => { 
     cy.wait(1000);
     //When I go section pages
     adminPage.clickPostBtn()  
-    screenShots.screenShot();
     cy.wait(1000);
     postManage.selectFirst();
-    screenShots.screenShot();
     cy.wait(1000);
     //And I enter title
     postManage.fillTitle('PostM');
-    screenShots.screenShot();
     //And I back to list Post
-    postManage.clickP()
-    screenShots.screenShot();
-    postManage.backPost()
-    screenShots.screenShot();
-    //I expect have to post with the same name "Post1"
+    postManage.clickP();
+    cy.wait(1000);
+    cy.go(-1)
+    postManage.validateInList('PostM')
     cy.wait(1000);
   })
 
@@ -112,45 +101,16 @@ context("Navigation", () => {
     cy.wait(1000);
     //When I go section pages
     adminPage.clickPostBtn()  
-    screenShots.screenShot();
     cy.wait(1000);  
     postManage.selectFirst();
-    screenShots.screenShot();
     cy.wait(1000);
     //And I got to setting
     postManage.seeSettings()
-    screenShots.screenShot();
     //And I delete Post
     postManage.deletePost()
-    screenShots.screenShot();
     postManage.confirmDeletePost()
-    screenShots.screenShot();
     
     cy.wait(1000);
   })
-
-
-
-  it('Scenario: No permitir guardar borrador de post con titulo vacio', () => { 
-    cy.wait(1000);
-    //When I go section Post
-    adminPage.clickPostBtn()
-    screenShots.screenShot();
-    cy.wait(1000);
-    postManage.createNewButton()
-    screenShots.screenShot();
-    cy.wait(1000);
-    //And I back to list Post
-    postManage.backPost()
-    screenShots.screenShot();
-    cy.wait(1000);
-    //And I go to Draf
-    postManage.backDarfPost()
-    screenShots.screenShot();
-    cy.wait(1000);
-  })
-
-  
-
 
 });
